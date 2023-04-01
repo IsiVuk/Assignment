921e4ac55404628ddb5458b03d9d490719f52c29
IsiVuk <isi.vukovic@gmail.com>
2023-04-01 18:22:04 +0200

    Commit for Assignment

---
+++ IsidoraV.py
@@ -0,0 +1,5 @@
+Python 3.11.1 (tags/v3.11.1:a7a450f, Dec  6 2022, 19:58:39) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> pozdrav = "Hello World"
+>>> print(pozdrav * 36)
+Hello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello WorldHello World
